9915aaccfdaa969e2aebae08fe45a6e88e40c3bb
Nikhil <47372038+nnetraga@users.noreply.github.com>
2019-03-17 20:29:00 -0700

    Initial commit

---
+++ WeatherApp/bin/Debug/AppX/js/main.js
@@ -0,0 +1,46 @@
+﻿// Your code here!
+const apikey = "3c82d2666238cd412b9d0dbc99fc719b";
+
+let searchButton = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city-name");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temp");
+let humidity = document.getElementById("humidity-div");
+
+searchButton.addEventListener("click", findWeatherDetails);
+searchInput.addEventListener("keyup", enterPressed);
+
+function enterPressed(event) {
+    if (event.key === "Enter") {
+        findWeatherDetails();
+    }
+}
+function findWeatherDetails() {
+    if (searchInput.nodeValue === "") {
+
+    }
+    else {
+        let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid=" + apikey+"units=imperial";
+        httpRequestAsync(searchLink, Response);
+    }
+}
+
+function Response(response) {
+    let jsonObject = JSON.parse(response);
+    cityName.innerHTML = jsonObject.name;
+    icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+    temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
+    humidity.innerHTML = jsonObject.main.humidity + "%";
+}
+
+function httpRequestAsync(url, callback) {
+    console.log("hello");
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+    }
+    httpRequest.open("GET", url, true); // true for asynchronous 
+    httpRequest.send();
+}
